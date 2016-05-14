b929e92165284908a825a70ddd48ff5741695b31
Johannes Griesser <ididnotinhale@gmail.com>
2016-05-14 13:35:30 +0100

    Included tmux symlink

--- symlinks.sh
+++ symlinks.sh
@@ -8,3 +8,7 @@ ln -sf ~/dotfiles/.vim/.vimrc.bundles ~/.vimrc.bundles
 # Neovim
 ln -sf ~/dotfiles/.vim ~/.config/nvim
 ln -sf ~/dotfiles/.vimrc ~/.config/nvim/init.vim
+
+# Tmux
+ln -sf ~dotfiles/.tmux.conf ~/.tmux.conf
+
