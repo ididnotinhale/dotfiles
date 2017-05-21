e7ea15e1c74f4877f09e5c075b84e58b16381b57
Johannes Griesser <johannes.griesser@nbcuni.com>
2017-05-21 01:13:53 +0100

    Renamed files and folders to avoid sourcing issues with vim

--- symlinks.sh
+++ symlinks.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/.dotfiles/bashrc ~/.bashrc
 
 # Vim
-ln -sf ~/dotfiles/.vim/ ~/.vim
-ln -sf ~/dotfiles/.vim/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.vim/.vimrc.bundles ~/.vimrc.bundles
+ln -sf ~/.dotfiles/vim ~/.vim
+ln -sf ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -sf ~/.dotfiles/vim/vimrc.bundles ~/.vimrc.bundles
 
 # Neovim
-ln -sf ~/dotfiles/.vim ~/.config/nvim
-ln -sf ~/dotfiles/.vimrc ~/.config/nvim/init.vim
+ln -sf ~/.dotfiles/vim ~/.config/nvim
+ln -sf ~/.dotfiles/vimrc ~/.config/nvim/init.vim
 
 # Tmux
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
